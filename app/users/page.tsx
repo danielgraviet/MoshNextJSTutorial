0184e00179274d68f85d1a70e91b2b859822b885
danielgraviet <dgravs5@gmail.com>
2024-11-21 20:59:59 -0700

    added a loading UI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserTable from './new/UserTable'
 import { sort } from 'fast-sort'
 import Link from 'next/link'
@@ -12,7 +12,9 @@ const UsersPage = async ({searchParams: {sortOrder}}: Props) => {
     <>
     <h1>User</h1>
     <Link href="/users/new" className="btn btn-primary">New User</Link>
+    <Suspense fallback={<p>Loading...</p>}>
     <UserTable sortOrder={sortOrder}/>
+    </Suspense>
     </>
   )
 }
