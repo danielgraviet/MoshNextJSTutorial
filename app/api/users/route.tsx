c9e3f51675f1f483fa401822c67823a9aa3eccad
danielgraviet <dgravs5@gmail.com>
2024-11-23 15:10:49 -0700

    practiced creating and object, using postman.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -14,3 +14,9 @@ export function GET(request: NextRequest) {
         ]
     );
 }
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    if(!body.name) return NextResponse.json({error: "name is required"}, {status:400})
+    return NextResponse.json({id: 1, name:body.name}, {status: 201});
+}
