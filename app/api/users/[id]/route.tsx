e2cc33354a50f0bf326e5b62848781cd3677ea53
danielgraviet <dgravs5@gmail.com>
2024-11-24 15:40:45 -0700

    added a function to delete an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -11,5 +11,38 @@ export function GET(request: NextRequest,
             {status: 404}
         )
         return NextResponse.json({id: 1, name: "danny"})
+}
 
+export async function PUT(request: NextRequest,  {params}: {params: {id: number}}) {
+    // validate the request body
+    const body = await request.json();
+    if (!body.name) {
+        // if invalid, return 400
+        return NextResponse.json({error: "Name is required"}, {status: 400})
+    }
+    
+    // fetch the user with the given id
+    if (params.id > 10) {
+        // if doesn't exist, return 404
+        return NextResponse.json({error: "User is required"}, {status: 404})
+    }
+
+    return NextResponse.json({id: 1, name: body.name})
+    //  update the user
+    // return the updated user 
+}
+
+export function DELETE(
+    request: NextRequest,
+    {params}: {params: {id: number}}
+) {
+    if (params.id > 10){
+        return NextResponse.json({error: "User not found"}, {status: 404})
+    }
+
+    return NextResponse.json({});
+    // fetch user from data base
+    // if not found, return 404 error
+    // if found, delete user
+    // return 200.
 }
