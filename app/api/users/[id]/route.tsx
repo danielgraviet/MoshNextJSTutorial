85aa0f6e279c9375f983c0b5fc151e8728745d58
danielgraviet <dgravs5@gmail.com>
2024-12-03 21:44:08 -0700

    created a method to update data. POST function.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -18,7 +18,7 @@ export async function GET(request: NextRequest,
         return NextResponse.json(user)
 }
 
-export async function PUT(request: NextRequest,  {params}: {params: {id: number}}) {
+export async function PUT(request: NextRequest,  {params}: {params: {id: string}}) {
     // validate the request body
     const body = await request.json();
     const validation = schema.safeParse(body);
@@ -27,15 +27,24 @@ export async function PUT(request: NextRequest,  {params}: {params: {id: number}
         return NextResponse.json(validation.error.errors, {status: 400})
     }
     
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(params.id)}
+    })
     // fetch the user with the given id
-    if (params.id > 10) {
+    if (!user) {
         // if doesn't exist, return 404
-        return NextResponse.json({error: "User is required"}, {status: 404})
+        return NextResponse.json({error: "User not found"}, {status: 404})
     }
 
-    return NextResponse.json({id: 1, name: body.name})
-    //  update the user
-    // return the updated user 
+    const updatedUser = await prisma.user.update({
+        where: {id: user.id},
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+
+    return NextResponse.json(updatedUser)
 }
 
 export function DELETE(
