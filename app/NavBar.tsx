a47e7c726c06ec087da15f3497d45a76aae19b4a
danielgraviet <dgravs5@gmail.com>
2024-12-14 10:36:56 -0700

    started configuring google provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex bg-slate-200 p-5'>
+    <div className='flex bg-slate-200 p-5 space-x-3'>
         <Link href="/" className="mr-5">Next.js</Link>
-        <Link href="/users" className="mr-5">Users</Link>
+        <Link href="/api/auth/signin" className="mr-5">Login</Link>
         <Link href="/admin" className="mr-5">Admin</Link>
     </div>
   )
